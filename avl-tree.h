75bbb6a54d660d16e2350fcc37d77bf87a21c5d5
phflrs <48444383+phflrs@users.noreply.github.com>
2019-05-28 11:04:02 -0300

    Add files via upload

---
+++ avl-tree.h
@@ -0,0 +1,14 @@
+/*cabeçalho das funções e estruturas de dados*/
+
+typedef struct nodo
+{
+    int chave;
+    struct nodo*  esq;
+    struct nodo*  dir;
+    int altura;
+} nodo;
+
+//nodo* busca( int chave, nodo *t );
+//      insere();
+//      remove();
+
